b00aac22194c7ed04c76f1319460df6c9d9113e8
TTtian1998 <1210059544@qq.com>
2023-04-26 23:51:10 +0800

    update demo

--- source/main.go
+++ source/main.go
@@ -7,23 +7,34 @@ import (
 
 func main() {
 	r := gweb.New()
-	r.GET("/", func(c *gweb.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	r.GET("/index", func(c *gweb.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *gweb.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		})
 
-	r.GET("/hello", func(c *gweb.Context) {
-		// expect /hello?name=tzx
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
+		v1.GET("/hello", func(c *gweb.Context) {
+			// expect /hello?name=geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *gweb.Context) {
+			// expect /hello/geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *gweb.Context) {
+			c.JSON(http.StatusOK, gweb.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
 
-	r.GET("/hello/:name", func(c *gweb.Context) {
-		// expect /hello/tzx
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
-
-	r.GET("/assets/*filepath", func(c *gweb.Context) {
-		c.JSON(http.StatusOK, gweb.H{"filepath": c.Param("filepath")})
-	})
+	}
 
 	r.Run(":9999")
 }
