037fcefcfdf242c40dc277ab69584f600f51faa0
TTtian1998 <1210059544@qq.com>
2023-04-27 23:59:20 +0800

    new demo

--- source/main.go
+++ source/main.go
@@ -2,38 +2,36 @@ package main
 
 import (
 	"gweb"
+	"log"
 	"net/http"
+	"time"
 )
+
+func onlyForV2() gweb.HandlerFunc {
+	return func(c *gweb.Context) {
+		// Start timer
+		t := time.Now()
+		// if a server error occurred
+		c.Fail(500, "Internal Server Error")
+		// Calculate resolution time
+		log.Printf("[%d] %s in %v for group v2", c.StatusCode, c.Req.RequestURI, time.Since(t))
+	}
+}
 
 func main() {
 	r := gweb.New()
-	r.GET("/index", func(c *gweb.Context) {
-		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
+	r.Use(gweb.Logger()) // global midlleware
+	r.GET("/", func(c *gweb.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 	})
-	v1 := r.Group("/v1")
-	{
-		v1.GET("/", func(c *gweb.Context) {
-			c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
-		})
 
-		v1.GET("/hello", func(c *gweb.Context) {
-			// expect /hello?name=geektutu
-			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-		})
-	}
 	v2 := r.Group("/v2")
+	v2.Use(onlyForV2()) // v2 group middleware
 	{
 		v2.GET("/hello/:name", func(c *gweb.Context) {
 			// expect /hello/geektutu
 			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
 		})
-		v2.POST("/login", func(c *gweb.Context) {
-			c.JSON(http.StatusOK, gweb.H{
-				"username": c.PostForm("username"),
-				"password": c.PostForm("password"),
-			})
-		})
-
 	}
 
 	r.Run(":9999")
