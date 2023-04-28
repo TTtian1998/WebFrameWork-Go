9f9d6e15dcbe2074893bc2b2b7fedd0767affe54
TTtian1998 <1210059544@qq.com>
2023-04-28 23:55:03 +0800

    update demo

--- source/main.go
+++ source/main.go
@@ -1,38 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"gweb"
-	"log"
+	"html/template"
 	"net/http"
 	"time"
 )
 
-func onlyForV2() gweb.HandlerFunc {
-	return func(c *gweb.Context) {
-		// Start timer
-		t := time.Now()
-		// if a server error occurred
-		c.Fail(500, "Internal Server Error")
-		// Calculate resolution time
-		log.Printf("[%d] %s in %v for group v2", c.StatusCode, c.Req.RequestURI, time.Since(t))
-	}
+type student struct {
+	Name string
+	Age  int8
+}
+
+func FormatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
 }
 
 func main() {
 	r := gweb.New()
-	r.Use(gweb.Logger()) // global midlleware
+	r.Use(gweb.Logger())
+	r.SetFuncMap(template.FuncMap{
+		"FormatAsDate": FormatAsDate,
+	})
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/assets", "./static")
+
+	stu1 := &student{Name: "Brown", Age: 20}
+	stu2 := &student{Name: "Jack", Age: 22}
 	r.GET("/", func(c *gweb.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		c.HTML(http.StatusOK, "css.tmpl", nil)
+	})
+	r.GET("/students", func(c *gweb.Context) {
+		c.HTML(http.StatusOK, "arr.tmpl", gweb.H{
+			"title":  "gweb",
+			"stuArr": [2]*student{stu1, stu2},
+		})
 	})
 
-	v2 := r.Group("/v2")
-	v2.Use(onlyForV2()) // v2 group middleware
-	{
-		v2.GET("/hello/:name", func(c *gweb.Context) {
-			// expect /hello/geektutu
-			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	r.GET("/date", func(c *gweb.Context) {
+		c.HTML(http.StatusOK, "custom_func.tmpl", gweb.H{
+			"title": "gweb",
+			"now":   time.Date(2019, 8, 17, 0, 0, 0, 0, time.UTC),
 		})
-	}
+	})
 
 	r.Run(":9999")
 }
