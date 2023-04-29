7c8599aa12d58ded94282c976bb02362193d2f88
TTtian1998 <1210059544@qq.com>
2023-04-29 22:46:13 +0800

    add Default func to demo

--- source/main.go
+++ source/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"gweb"
-	"html/template"
 	"net/http"
 	"time"
 )
@@ -19,32 +18,21 @@ func FormatAsDate(t time.Time) string {
 }
 
 func main() {
-	r := gweb.New()
-	r.Use(gweb.Logger())
-	r.SetFuncMap(template.FuncMap{
-		"FormatAsDate": FormatAsDate,
-	})
-	r.LoadHTMLGlob("templates/*")
-	r.Static("/assets", "./static")
-
-	stu1 := &student{Name: "Brown", Age: 20}
-	stu2 := &student{Name: "Jack", Age: 22}
+	//use Default to demonstrate customize middleware and print trace message
+	//net/http also contains their panic handler, so when there is a panic when using gweb.New
+	//the app won't terminate
+	r := gweb.Default()
 	r.GET("/", func(c *gweb.Context) {
-		c.HTML(http.StatusOK, "css.tmpl", nil)
+		c.String(http.StatusOK, "Hello GWeb\n")
 	})
-	r.GET("/students", func(c *gweb.Context) {
-		c.HTML(http.StatusOK, "arr.tmpl", gweb.H{
-			"title":  "gweb",
-			"stuArr": [2]*student{stu1, stu2},
-		})
+	// index out of range for testing Recovery()
+	r.GET("/panic", func(c *gweb.Context) {
+		names := []string{"gweb"}
+		c.String(http.StatusOK, names[100])
 	})
-
-	r.GET("/date", func(c *gweb.Context) {
-		c.HTML(http.StatusOK, "custom_func.tmpl", gweb.H{
-			"title": "gweb",
-			"now":   time.Date(2019, 8, 17, 0, 0, 0, 0, time.UTC),
-		})
+	r.GET("/panic", func(c *gweb.Context) {
+		names := []string{"gweb"}
+		c.String(http.StatusOK, names[100])
 	})
-
 	r.Run(":9999")
 }
